9a21f79e42dc242fed4249661430c07b9f0041a9
Steve <steve@kitkat@gmail.com>
2024-10-02 14:19:13 +0700

    Can load the poster images

--- composeApp/src/commonMain/kotlin/me/androidbox/busbymoviesv2/move_list/presentation/screens/MovieListScreen.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/busbymoviesv2/move_list/presentation/screens/MovieListScreen.kt
@@ -1,5 +1,6 @@
 package me.androidbox.busbymoviesv2.move_list.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,9 +18,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import busbymoviesv2.composeapp.generated.resources.Res
+import busbymoviesv2.composeapp.generated.resources.compose_multiplatform
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import me.androidbox.busbymoviesv2.move_list.presentation.MovieListState
+import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -44,10 +48,19 @@ fun MovieListScreen(
                     }
                 ) { movieResult ->
                     Column {
-                        KamelImage({ asyncPainterResource(data = "https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg") },
+                        KamelImage({ asyncPainterResource(data = "https://image.tmdb.org/t/p/w500${movieResult.posterPath}") },
                             contentDescription = "",
                             modifier = Modifier.width(100.dp).height(200.dp),
                             contentScale = ContentScale.Crop,
+                            onLoading = {progress ->
+                                CircularProgressIndicator(
+                                    progress = progress,
+                                    color = Color.Blue
+                                )
+                            },
+                            onFailure = {
+                                Image(imageVector = vectorResource(Res.drawable.compose_multiplatform), contentDescription = null)
+                            }
                           )
 
                          Text(
