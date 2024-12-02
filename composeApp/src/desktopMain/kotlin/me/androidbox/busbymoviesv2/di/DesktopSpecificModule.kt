aa7d393cd6bac622883bb5764afc5b41b1e56d96
Steve <steve@kitkat@gmail.com>
2024-12-02 20:23:55 +0700

    Adds the room database creation in the specifie module

--- composeApp/src/desktopMain/kotlin/me/androidbox/busbymoviesv2/di/DesktopSpecificModule.kt
+++ composeApp/src/desktopMain/kotlin/me/androidbox/busbymoviesv2/di/DesktopSpecificModule.kt
@@ -1,12 +1,25 @@
 package me.androidbox.busbymoviesv2.di
 
+import androidx.room.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
+import me.androidbox.busbymoviesv2.movie_details.data.entities.MovieDetailDatabase
 import org.koin.dsl.module
+import java.io.File
 
 val desktopSpecificModule = module {
     single<HttpClientEngine> {
         HttpClient(CIO).engine
+    }
+
+    single<MovieDetailDatabase> {
+        val dbFile = File(System.getProperty("java.io.tmpdir"), "movieDetail.db.db")
+        
+        Room.databaseBuilder<MovieDetailDatabase>(
+            name = dbFile.absolutePath)
+            .setDriver(BundledSQLiteDriver())
+            .build()
     }
 }
--- composeApp/src/nativeMain/kotlin/me/androidbox/busbymoviesv2/di/nativeModule.kt
+++ composeApp/src/nativeMain/kotlin/me/androidbox/busbymoviesv2/di/nativeModule.kt
@@ -4,24 +4,26 @@ import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.darwin.Darwin
 import me.androidbox.busbymoviesv2.movie_details.data.entities.MovieDetailDatabase
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
-val androidModule = module {
+val nativeModule = module {
     single<HttpClientEngine> {
         /** As we are using specific engines,
          * might as well use the specific ones for each platform i.e android, iso, desktop, js */
-        HttpClient(CIO).engine
+        HttpClient(Darwin).engine
     }
 
     single<MovieDetailDatabase> {
-        val dbFile = androidContext().getDatabasePath("movieDetail.db")
+        val dbFile = NSHomeDirectory() + "/movieDetail.db"
 
-        Room.databaseBuilder<MovieDetailDatabase>(
-            context = androidContext(),
-            name = dbFile.absolutePath)
+        return Room.databaseBuilder<MovieDetailDatabase>(
+            name = dbFile,
+            factory = {
+                MovieDetailDatabase::class.instantiateImpl()
+            }
+        )
             .setDriver(BundledSQLiteDriver())
             .build()
     }
