5bcf0db4e45d6f434972c659de5a4a78883ad8cc
Steve <steve@kitkat@gmail.com>
2024-12-06 13:11:44 +0700

    Show message when there are no favourites

--- composeApp/src/commonMain/kotlin/me/androidbox/busbymoviesv2/favourites/presentation/screens/FavouriteScreen.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/busbymoviesv2/favourites/presentation/screens/FavouriteScreen.kt
@@ -1,10 +1,16 @@
 package me.androidbox.busbymoviesv2.favourites.presentation.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import me.androidbox.busbymoviesv2.movie_details.domain.models.MovieFavouriteModel
 
 @Composable
@@ -13,22 +19,34 @@ fun FavouriteScreen(
     onMovieClicked: (movieId: Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier = modifier.fillMaxWidth()
-    ) {
-        items(
-            key = { movieFavouriteModel ->
-                movieFavouriteModel.id
-            },
-            items = listOfFavouriteMovies,
-            itemContent = { movieFavouriteModel ->
-                FavouriteMovieItem(
-                    movieFavouriteModel,
-                    onMovieClicked = { movieId ->
-                        onMovieClicked(movieId)
+
+    Box(modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+
+        if(listOfFavouriteMovies.isEmpty()) {
+            Text(text = "No favourites have been added",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold)
+        }
+        else {
+            LazyColumn(
+                modifier = modifier.fillMaxSize()
+            ) {
+                items(
+                    key = { movieFavouriteModel ->
+                        movieFavouriteModel.id
+                    },
+                    items = listOfFavouriteMovies,
+                    itemContent = { movieFavouriteModel ->
+                        FavouriteMovieItem(
+                            movieFavouriteModel,
+                            onMovieClicked = { movieId ->
+                                onMovieClicked(movieId)
+                            }
+                        )
                     }
                 )
             }
-        )
+        }
     }
 }
